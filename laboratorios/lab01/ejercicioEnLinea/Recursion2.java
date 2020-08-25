98df06be91004d80d88e6ffe0497bb2be86ae916
tnietov <68928352+tnietov@users.noreply.github.com>
2020-08-24 22:57:51 -0500

    Update Recursion2.java

--- laboratorios/lab01/ejercicioEnLinea/Recursion2.java
+++ laboratorios/lab01/ejercicioEnLinea/Recursion2.java
@@ -58,3 +58,19 @@ public boolean groupSumClump(int start, int[] nums, int target) {
     else return groupSumClump(start+1,nums,target-nums[start])|| groupSumClump(start+1,nums,target);
   }
 }
+
+
+
+
+
+public boolean splitArray(int[] nums) {
+  return helper(0,nums,0,0);
+}
+private boolean helper(int start, int[] nums,int sum1, int sum2){
+  if(start>=nums.length){
+    if(sum1==sum2)return true;
+    else return false;
+  }else{
+    return helper(start+1,nums,sum1+nums[start],sum2)||helper(start+1,nums,sum1,sum2+nums[start]);
+  }
+}
