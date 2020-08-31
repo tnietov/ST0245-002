dd4b4c91b121fbfd1903d4266a8a368fe25c1d80
tnietov <68928352+tnietov@users.noreply.github.com>
2020-08-31 12:45:10 -0500

    Update Taller05.java

--- talleres/taller05/Taller05.java
+++ talleres/taller05/Taller05.java
@@ -1,30 +1,26 @@
 public class Insertion_sort
 {
    public void InserstionSort(int[] arreglo){
-      int i , y ,temp;// c_1
+      int i , y ,temp;
 
-      for(i=1;i<arreglo.length;i++){   //C_2+t(n-1)
-         y= i-1; //C_3t
-        while(arreglo[y]>arreglo[i] && y>=0 ){ // C_4 +T(n-1)
-            arreglo[y+1]=arreglo[y];//// C_5
-            y= y-1; //C_6
+      for(i=1;i<arreglo.length;i++){   
+         y= i-1; 
+        while(arreglo[y]>arreglo[i] && y>=0 ){ 
+            arreglo[y+1]=arreglo[y];
+            y= y-1;
        }
-        arreglo[y+1] = arreglo[i]; //C_7
-     
+        arreglo[y+1] = arreglo[i];
     }
-           //T(n)=C_2+T(n-1)+C_4+T(n-1)
   }   
 }
 
 public class Enigma
 {
     public void suma(int[] n){
-        int x=0,i;//C_1
-        
-        for(i=0;i<n.length;i++){//C_2 + T(n-1)
-            x+=n[i];//C_3
-            System.out.println("Variable sumo: "+ x);//C_4
+        int x=0,i;
+        for(i=0;i<n.length;i++){
+            x+=n[i];
+            System.out.println("Variable sumo: "+ x);
         }
-       
     }
 }
